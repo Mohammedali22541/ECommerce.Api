0d8fe77da8a68ee45bcc6ea3f59abcebf0bb1379
Mohammedali22541 <Mohammedali792406@gmail.com>
2025-11-07 04:33:53 +0200

    Revert "S2"

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -1,47 +1,20 @@
 
-using Ecommerce.Api.Extensions;
-using ECommerce.Domain.Contracts;
-using ECommerce.Persistence.Data.DataSeed;
-using ECommerce.Persistence.Data.DbContexts;
-using ECommerce.Persistence.Data.Repositories;
-using ECommerce.Services;
-using ECommerce.Services.Abstraction;
-using ECommerce.Services.MappingProfiles;
-using Microsoft.EntityFrameworkCore;
-
 namespace Ecommerce.Api
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            #region Register Container
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            builder.Services.AddDbContext<StoreDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-            builder.Services.AddScoped<IDataIntializer, DataIntializier>();
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddAutoMapper(x=>x.AddProfile<ProductProfile>());
-            builder.Services.AddScoped<IProductService, ProductServices>();
-            #endregion
-
             var app = builder.Build();
-
-            await app.MigrateDatabaseAsync();
-            await app.SeedDataAsync();
-
-
-            #region Configure PipeLine
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -55,10 +28,9 @@ namespace Ecommerce.Api
             app.UseAuthorization();
 
 
-            app.MapControllers(); 
-            #endregion
+            app.MapControllers();
 
-            await app.RunAsync();
+            app.Run();
         }
     }
 }
